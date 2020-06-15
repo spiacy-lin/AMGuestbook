3bf12086dca31be26a10e5ba798be6476b3df21c
Andrzej Malik <andrzejkom2016@gmail.com>
2020-06-15 12:15:01 +0200

    Add project files.

---
+++ Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMGuestbook.Models
+{
+    public class Guest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public string CurrentText { get; set; }
+        public string Name { get; set; }
+    }
+}
