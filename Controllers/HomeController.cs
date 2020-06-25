2eb1a98f0e6e807324716eebe8ba2646d8f925a1
Andrzej Malik <andrzejkom2016@gmail.com>
2020-06-25 10:42:15 +0200

    New page LOgin

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace AMGuestbook.Controllers
         {
             return View();
         }
-
+ 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
